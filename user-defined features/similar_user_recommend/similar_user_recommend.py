0d61a56ed54619dd6b3e2707ba1be6dad02928be
jiaying <55706191+jiaying777@users.noreply.github.com>
2020-05-19 23:17:29 +0800

    Update similar_user_recommend.py

--- user-defined features/similar_user_recommend/similar_user_recommend.py
+++ user-defined features/similar_user_recommend/similar_user_recommend.py
@@ -6,8 +6,11 @@ class subscription_list_recommendation:
     def __init__(self):
         self.weight_user_industry = pd.read_csv('weight_user_industry.csv')
         self.user_subscribed = pd.read_csv('subscribe_wl.csv')
-        
+     
     def userid(self,user_id):
+        '''
+        找出最相近的5個使用者，同時找最相近的6~10，避免最相近的5個使用者的訂閱清單重複性過高
+        '''
         data_user = self.weight_user_industry.loc[user_id,'ETF':]
         data = self.weight_user_industry.drop(user_id)
         data['dist'] = data.loc[:,'ETF':].apply(lambda x: np.sqrt(np.sum(np.square(data_user - x))),axis=1)
@@ -18,6 +21,9 @@ class subscription_list_recommendation:
         return user_id,id2,id6_10
 
     def subscribed(self,id1,id2):
+        '''
+        找出最相近的5個使用者清單，並篩選掉已訂閱的股票，並從剩下的訂閱中隨機挑選5檔股票推薦
+        '''
         data_id1 = self.user_subscribed[self.user_subscribed['user_id_y'] == id1]
         data_id1['lists'] = data_id1['lists'].apply(lambda x: x[2:-2].split("', '"))      
         data = self.user_subscribed[(self.user_subscribed['user_id_y'] == id2[0]) | (self.user_subscribed['user_id_y'] == id2[1]) | 
